2176ec7f1aa9b2477339fb924e62d01628dfc40a
NabinShahi <nabin.shahi4312@gmail.com>
2020-08-31 09:55:46 +0545

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//passing images as props  -> JS literals
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Team() {
+  return(
+    <>
+    <center><h1 className =  "heading"><span className =  "ball1" style = {{color: "blue"}}>
+      <i class="fa fa-futbol-o" aria-hidden="true"></i>  </span>
+      Barcelona Football Club  <span className =  "ball2" style = {{color: "red"}}>
+        <i class="fa fa-futbol-o" aria-hidden="true"></i>  </span></h1></center>
+    <section className = "name">
+      <Player img = "1" name = " Griezmann" position = "Forward" link = "https://www.instagram.com/antogriezmann/"/>
+      <Player img = "2" name = " Iniesta" position = "Mid-fielder" link = "https://www.instagram.com/andresiniesta8/"/>
+      <Player img = "3" name = "Messi" position = "Forward" link = "https://www.instagram.com/leomessi/"/>
+      <Player img = "4" name = "Ronaldinho" position = "winger" link = "https://www.facebook.com/ronaldinho/"/>
+      <Player img = "5" name = "Ter Stegen" position = "Keeper" link = "https://www.instagram.com/mterstegen1/"/>
+      <Player img = "6" name = "Xavi" position = "Mid-fielder" link = "https://www.instagram.com/xavi/"/>
+    </section>
+    <Sign />
+    </>
+  )
+}
+ const Player = ({img, name, position, link}) => {
+   let image = `./Images/${img}.jpg`
+   return(
+     <div className = "list">
+       <img src = {image} alt = "player" />
+       <center>
+        <h1>{name}</h1>
+        <h2>{position}</h2>
+        <a href = {link}><button className = "about">More Details</button></a>
+       </center>
+     </div>
+   )
+ }
+
+const Sign = () => {
+  return( 
+    <div className = "copyright">
+      <p>By Nabin Shahi</p>
+    </div>
+    )
+  }
+
+ReactDom.render(<Team />, document.getElementById("root"));
